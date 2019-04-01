5701030f2d328d889b701071dcb80546fc482827
LewisMartin <Lewis_J_Martin@hotmail.co.uk>
2019-04-01 23:09:18 +0100

    Add first test to check Selenium config

---
+++ BirdwatchVisualizerUITests/Tests/OverviewPageTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace BirdwatchVisualizerUITests.Tests
+{
+    [TestFixture]
+    class OverviewPageTests
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            StartBrowser();
+        }
+
+        [Test]
+        public void NavigateToHomePage()
+        {
+            driver.Navigate().GoToUrl("http://localhost:4200");
+
+            Assert.AreEqual(driver.Title, "Birdwatch-Visualizer");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            CloseBrowser();
+        }
+
+        private void StartBrowser()
+        {
+            driver = new ChromeDriver(@"D:\Tools\Selenium\ChromeDriver\chromedriver_win32\");
+        }
+
+        private void CloseBrowser()
+        {
+            driver.Close();
+        }
+    }
+}
