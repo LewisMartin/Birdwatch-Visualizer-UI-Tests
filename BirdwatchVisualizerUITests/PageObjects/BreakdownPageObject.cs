662a086f2957058d07925518fb41c7b8b3d5590a
LewisMartin <Lewis_J_Martin@hotmail.co.uk>
2019-04-05 00:56:35 +0100

    Add breakdown page test fixture

---
+++ BirdwatchVisualizerUITests/PageObjects/BreakdownPageObject.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BirdwatchVisualizerUITests.PageObjects
+{
+    public class BreakdownPageObject
+    {
+        public static string URL = "http://localhost:4200/breakdown";
+
+        public static By ControlBar = By.ClassName("control-bar");
+
+        public static By BreakdownStatsContainer = By.ClassName("breakdown-stats");
+
+        public static By ChartContainer = By.ClassName("canvas-container");
+    }
+}
---
+++ BirdwatchVisualizerUITests/Tests/BreakdownPageTestFixture.cs
@@ -0,0 +1,58 @@
+﻿using BirdwatchVisualizerUITests.Attributes;
+using BirdwatchVisualizerUITests.PageObjects;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace BirdwatchVisualizerUITests.Tests
+{
+    [TestFixture]
+    public class BreakdownPageTestFixture : SeleniumTestFixtureBase
+    {
+        public override void SetUp()
+        {
+            base.SetUp();
+        }
+
+        public override void TearDown()
+        {
+            base.TearDown();
+        }
+
+        [Test]
+        public void WhenBreakdownPageLoads_YearDropDownMenuIsRendered([BrowserList] BrowserTypes browser)
+        {
+            Initialize(browser);
+
+            WebDriver.Navigate().GoToUrl(BreakdownPageObject.URL);
+            var controlBar = WebDriver.FindElement(BreakdownPageObject.ControlBar);
+            var dropDown = controlBar.FindElement(By.TagName("mat-select"));
+
+            Assert.IsNotNull(dropDown.Text);
+            Assert.IsNotEmpty(dropDown.Text);
+        }
+
+        [Test]
+        public void WhenBreakdownPageLoads_BreakdownStatsAreRendered([BrowserList] BrowserTypes browser)
+        {
+            Initialize(browser);
+
+            WebDriver.Navigate().GoToUrl(BreakdownPageObject.URL);
+            var controlBar = WebDriver.FindElement(BreakdownPageObject.BreakdownStatsContainer);
+
+            Assert.IsTrue(controlBar.Text.ToLower().Contains("total birds") &&
+                        controlBar.Text.ToLower().Contains("different species"));
+        }
+
+        [Test]
+        public void WhenBreakdownPageLoads_ChartCanvasIsRendered([BrowserList] BrowserTypes browser)
+        {
+            Initialize(browser);
+
+            WebDriver.Navigate().GoToUrl(BreakdownPageObject.URL);
+            var controlBar = WebDriver.FindElement(BreakdownPageObject.ChartContainer);
+            var chartCanvas = WebDriver.FindElement(By.TagName("canvas"));
+
+            Assert.IsNotNull(chartCanvas);
+        }
+    }
+}
