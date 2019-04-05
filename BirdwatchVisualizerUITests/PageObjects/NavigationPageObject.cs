73d6e520020e733e2b776ec71f6c44ceb21a0118
LewisMartin <Lewis_J_Martin@hotmail.co.uk>
2019-04-05 21:18:18 +0100

    Add navigation tests

---
+++ BirdwatchVisualizerUITests/PageObjects/NavigationPageObject.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+
+namespace BirdwatchVisualizerUITests.PageObjects
+{
+    public class NavigationPageObject
+    {
+        public static string URL = "http://localhost:4200";
+
+        public static By Hamburger = By.Id("hamburger");
+
+        public static By BreakdownLink = By.ClassName("breakdown-link");
+
+        public static By TrackerLink = By.ClassName("tracker-link");
+
+        public static By OverviewLink = By.ClassName("app-title");
+    }
+}
---
+++ BirdwatchVisualizerUITests/Tests/NavigationPageTestFixture.cs
@@ -0,0 +1,56 @@
+﻿using BirdwatchVisualizerUITests.Attributes;
+using BirdwatchVisualizerUITests.PageObjects;
+using NUnit.Framework;
+
+namespace BirdwatchVisualizerUITests.Tests
+{
+    [TestFixture]
+    public class NavigationPageTestFixture : SeleniumTestFixtureBase
+    {
+        public override void SetUp()
+        {
+            base.SetUp();
+        }
+
+        public override void TearDown()
+        {
+            base.TearDown();
+        }
+
+        [Test]
+        public void WhenNavigationLoads_ItContainsLinkToOverviewPage([BrowserList] BrowserTypes browser)
+        {
+            Initialize(browser);
+
+            WebDriver.Navigate().GoToUrl(NavigationPageObject.URL);
+            WebDriver.FindElement(NavigationPageObject.Hamburger).Click();
+            var link = WebDriver.FindElement(NavigationPageObject.OverviewLink);
+
+            Assert.IsTrue(link.GetAttribute("routerLink") == "/");
+        }
+
+        [Test]
+        public void WhenNavigationLoads_ItContainsLinkToBreakdownPage([BrowserList] BrowserTypes browser)
+        {
+            Initialize(browser);
+
+            WebDriver.Navigate().GoToUrl(NavigationPageObject.URL);
+            WebDriver.FindElement(NavigationPageObject.Hamburger).Click();
+            var link = WebDriver.FindElement(NavigationPageObject.BreakdownLink);
+
+            Assert.IsTrue(link.GetAttribute("routerLink") == "/breakdown");
+        }
+
+        [Test]
+        public void WhenNavigationLoads_ItContainsLinkToTrackerPage([BrowserList] BrowserTypes browser)
+        {
+            Initialize(browser);
+
+            WebDriver.Navigate().GoToUrl(NavigationPageObject.URL);
+            WebDriver.FindElement(NavigationPageObject.Hamburger).Click();
+            var link = WebDriver.FindElement(NavigationPageObject.TrackerLink);
+
+            Assert.IsTrue(link.GetAttribute("routerLink") == "/tracker");
+        }
+    }
+}
